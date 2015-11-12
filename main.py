8ecc83f50cd162b308ae5e8aa0726d3b167df473
Oleksandr Halushko <alexlesang@gmail.com>
2015-11-12 19:24:48 +0100

    added edge colors removed scaling

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from visualizator.vtkVisualizeGraph import VTKVisualizer
 
 if __name__ == '__main__':
     print(sys.argv)
-    if len(sys.argv) <= 1 or len(sys.argv) > 3:
-        print "Args should be: filename [max_num_of_vertices]"
+    if len(sys.argv) <= 1 or len(sys.argv) > 4:
+        print "Args should be: filename [max_num_of_vertices] [edge_colors_filename]"
 
     else:
-        if len(sys.argv) == 3:
+        if len(sys.argv) == 2:
+            v = VTKVisualizer(sys.argv[1])
+        elif len(sys.argv) == 3:
             v = VTKVisualizer(sys.argv[1], int(sys.argv[2]))
         else:
-            v = VTKVisualizer(sys.argv[1])
+            v = VTKVisualizer(sys.argv[1], int(sys.argv[2]), sys.argv[3])
 
         v.vizualize_grapth()
--- visualizator/vtkVisualizeGraph.py
+++ visualizator/vtkVisualizeGraph.py
@@ -5,6 +5,8 @@ from vtk.vtkRenderingCorePython import vtkGraphToGlyphs
 
 from visualizator.LGLReader import LGLReader
 
+EDGE_COLORS = "EdgeColors"
+
 VERTEX_ID = "VertexID"
 SCALINGS = "Scalings"
 WEIGHTS = "Weights"
@@ -12,12 +14,16 @@ LABELS = "Labels"
 
 
 class VTKVisualizer(object):
-    def __init__(self, filename, max_num_of_vertices=-1):
+    def __init__(self, filename, max_num_of_vertices=-1, edge_color_filename=None):
         super(VTKVisualizer, self).__init__()
+        self.edge_counter = 0
+        self.lookup_table = vtk.vtkLookupTable()
+        self.lookup_table.SetNumberOfColors(int(1e8))
+        self.edge_color_tuples = {}
+        self.edge_color_filename = edge_color_filename
         self.label_vertex_id_map = {}
         self.starting_vertex = None
         self.starting_vertex_index = -1
-        self.num_of_references = 0
         self.filename = filename
         self.max_num_of_vertices = max_num_of_vertices
         self.g = vtk.vtkMutableDirectedGraph()
@@ -38,32 +44,41 @@ class VTKVisualizer(object):
         self.edge_weights.SetNumberOfComponents(1)
         self.edge_weights.SetName(WEIGHTS)
 
+        self.edge_colors = vtk.vtkIntArray()
+        self.edge_colors.SetNumberOfComponents(1)
+        self.edge_colors.SetName(EDGE_COLORS)
+
     def vizualize_grapth(self):
         self.g.GetEdgeData().AddArray(self.edge_weights)
         self.g.GetVertexData().AddArray(self.labels)
         self.g.GetVertexData().AddArray(self.vertex_ids)
         self.g.GetVertexData().SetPedigreeIds(self.vertex_ids)
         self.g.GetVertexData().AddArray(self.glyph_scales)
+
+        if self.edge_color_filename:
+            self.read_edge_colors()
+            self.g.GetEdgeData().AddArray(self.edge_colors)
+
         self.insert_graph()
 
-        # num_of_tuples = self.glyph_scales.GetNumberOfTuples()
-        # for i in range(num_of_tuples):
-        #     scale = self.glyph_scales.GetTuple1(i)
-        #     print(scale)
-        #     # scale = 1.0 + 1.0/scale
-        #     # self.glyph_scales.SetTuple1(i, scale)
+        if self.edge_color_filename:
+            self.lookup_table.Build()
 
         graphLayoutView = vtk.vtkGraphLayoutView()
         graphLayoutView.AddRepresentationFromInput(self.g)
-        graphLayoutView.SetLayoutStrategy("Random")
+        graphLayoutView.SetLayoutStrategy("Span Tree")
         graphLayoutView.GetLayoutStrategy().SetEdgeWeightField(WEIGHTS)
         graphLayoutView.GetLayoutStrategy().SetWeightEdges(1)
 
         graphLayoutView.SetEdgeLabelArrayName(WEIGHTS)
-        graphLayoutView.SetEdgeLabelVisibility(1)
+        graphLayoutView.SetEdgeLabelVisibility(0)
+
+        if self.edge_color_filename:
+            graphLayoutView.SetEdgeColorArrayName(EDGE_COLORS)
+            graphLayoutView.ColorEdgesOn()
 
         graphLayoutView.SetVertexLabelArrayName(LABELS)
-        graphLayoutView.SetVertexLabelVisibility(1)
+        graphLayoutView.SetVertexLabelVisibility(0)
 
         graphLayoutView.ScaledGlyphsOn()
         graphLayoutView.SetScalingArrayName(SCALINGS)
@@ -72,7 +87,6 @@ class VTKVisualizer(object):
         graphLayoutView.GetRenderWindow().SetSize(1024, 768)
         graphLayoutView.ResetCamera()
         graphLayoutView.Render()
-        graphLayoutView.GetLayoutStrategy().SetRandomSeed(0)
         graphLayoutView.GetInteractor().SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
         graphLayoutView.GetInteractor().Start()
 
@@ -81,10 +95,13 @@ class VTKVisualizer(object):
             lgl = lgl_file.read()
 
         for i, entry in enumerate(lgl.split(os.linesep)):
+            if not entry:
+                continue
+
             if self.max_num_of_vertices != -1 and i > self.max_num_of_vertices:
                 break
 
-            self.glyph_scales.InsertNextValue(float(1.0))
+            self.glyph_scales.InsertNextValue(float(0.1))
 
             if LGLReader.is_starting_vertex(entry):
                 self._process_primary_vertex(entry, i)
@@ -96,19 +113,23 @@ class VTKVisualizer(object):
         secondary_label = self.get_secondary_vertex_label(entry)
         s_v = self._add_vertex(i, secondary_label)
         self.g.AddGraphEdge(self.starting_vertex, s_v)
-        self.num_of_references += 1
+        self.edge_counter += 1
+        if self.edge_color_filename:
+            key = (self.starting_vertex, s_v)
+            if key not in self.edge_color_tuples:
+                color = (1.0, 1.0, 1.0)
+            else:
+                color = self.edge_color_tuples[key]
+
+            self.lookup_table.SetTableValue(self.edge_counter, color[0], color[1], color[2])
+
+            self.edge_colors.InsertNextValue(self.edge_counter)
 
     def _process_primary_vertex(self, entry, i):
         primary_label = self._get_primary_label(entry)
         p_v = self._add_vertex(i, primary_label)
-        self._set_primary_vertex_scaling()
         self.starting_vertex = p_v
         self.starting_vertex_index = i
-        self.num_of_references = 0
-
-    def _set_primary_vertex_scaling(self):
-        if self.starting_vertex_index != -1:
-            self.glyph_scales.SetTuple1(self.starting_vertex_index, 0.5 * self.num_of_references)
 
     def _add_vertex(self, i, label):
         if label not in self.label_vertex_id_map:
@@ -140,3 +161,17 @@ class VTKVisualizer(object):
             secondary_label = label_weight
             self.edge_weights.InsertNextValue(0.5)
         return secondary_label
+
+    def read_edge_colors(self):
+        with open(self.edge_color_filename, 'rb') as edge_colors:
+            color_entries = edge_colors.read()
+
+        for i, entry in enumerate(color_entries.split(os.linesep)):
+            if not entry:
+                continue
+
+            t = LGLReader.get_edge_color_entry(entry)
+            if not t:
+                print('Bad color entry at' + str(i) + ': ' + str(t))
+            else:
+                self.edge_color_tuples[t[0]] = t[1]
